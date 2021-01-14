623141650acb667f9b4197e18b90b063f4c9c551
DavidN <davidnobles95@gmail.com>
2021-01-13 21:25:38 -0800

    raspberry pi comment

--- setup_manual.sh
+++ setup_manual.sh
@@ -2,6 +2,7 @@
 
 # install github cli (https://github.com/cli/cli/blob/trunk/docs/install_linux.md)
 # The "recv-key" (below) looks suspiciously private, but it's from the url above *shrugs*
+# note that raspberry pi is debian linux, so this will work on ubuntu and raspberry pis
 apt-key adv --keyserver keyserver.ubuntu.com --recv-key C99B11DEB97541F0
 apt-add-repository https://cli.github.com/packages
 apt update
